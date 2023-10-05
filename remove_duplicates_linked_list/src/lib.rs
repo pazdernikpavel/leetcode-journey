75ee94f6e4e5f7d1fb8b0245e8c17553da79c30d
Pavel Pazderník <pazdernik.pavel@email.cz>
2023-10-05 18:28:39 +0200

    Add remove linked list duplicates

--- remove_duplicates_linked_list/src/lib.rs
+++ remove_duplicates_linked_list/src/lib.rs
@@ -1,5 +1,12 @@
 use utils::singly_linked_list::ListNode;
 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Time complexity is tricky in this case due to use of 2 while loops, but at the end each element
+// is iterated only once, so that is why time complexity is O(n). Second while loop was necessary
+// in my opinion due to strict borrow checking rules, so it could not be done via if statement.
+
 pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut curr = &mut head;
     while let Some(node) = curr {
