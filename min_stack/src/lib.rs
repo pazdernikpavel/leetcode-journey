4cd60d07bd5d4320e3df2144cdcd62e9d8216cdb
Pavel Pazderník <pazdernik.pavel@email.cz>
2023-10-14 11:06:19 +0200

    Add min. stack

--- min_stack/src/lib.rs
+++ min_stack/src/lib.rs
@@ -2,7 +2,47 @@ use std::cell::RefCell;
 use std::cmp::min;
 use std::rc::{Rc, Weak};
 
-// TODO: https://leetcode.com/problems/min-stack/
+// pub struct MinStack {
+//     values: Vec<i32>,
+//     min_history: Vec<i32>,
+// }
+//
+// impl MinStack {
+//     pub fn new() -> Self {
+//         MinStack {
+//             values: vec![],
+//             min_history: vec![],
+//         }
+//     }
+//
+//     pub fn push(&mut self, val: i32) {
+//         self.values.push(val);
+//         if let Some(&last) = self.min_history.last() {
+//             if last >= val {
+//                 self.min_history.push(val);
+//             }
+//         } else {
+//             self.min_history.push(val);
+//         }
+//     }
+//
+//     pub fn pop(&mut self) {
+//         let removed = self.values.pop();
+//         if let (Some(removed_val), Some(&last_min_val)) = (removed, self.min_history.last()) {
+//             if removed_val == last_min_val {
+//                 self.min_history.pop();
+//             }
+//         }
+//     }
+//
+//     pub fn top(&self) -> i32 {
+//         self.values[self.values.len() - 1]
+//     }
+//
+//     pub fn get_min(&self) -> i32 {
+//         self.min_history[self.min_history.len() - 1]
+//     }
+// }
 
 #[derive(Debug)]
 pub struct Node {
@@ -44,23 +84,35 @@ impl MinStack {
             self.head = Some(Rc::clone(&new_node));
             self.tail = Some(Rc::clone(&new_node));
         } else if let Some(ref mut tail_node) = self.tail {
-            new_node.borrow_mut().min = min(tail_node.borrow().min, val);
+            // Update new node
+            let mut borrowed_node = new_node.borrow_mut();
+            borrowed_node.min = min(tail_node.borrow().min, val);
+            borrowed_node.prev = Some(Rc::downgrade(tail_node));
+            // Adjust stack tail
             tail_node.borrow_mut().next = Some(Rc::clone(&new_node));
-            new_node.borrow_mut().prev = Some(Rc::downgrade(tail_node));
             self.tail = Some(Rc::clone(&new_node));
         }
     }
 
-    // pub fn pop(&mut self) {}
+    pub fn pop(&mut self) {
+        if let Some(tail_node) = self.tail.as_ref() {
+            let prev = tail_node.borrow_mut().prev.take();
+            self.tail = prev.and_then(|weak| weak.upgrade()).map(|tail| {
+                tail.borrow_mut().next = None;
+                tail
+            });
+        }
+    }
 
-    //
-    // pub fn top(&self) -> i32 {
-    //     self.values[self.values.len() - 1]
-    // }
-    //
-    // pub fn get_min(&self) -> i32 {
-    //     self.min_history[self.min_history.len() - 1]
-    // }
+    pub fn top(&self) -> i32 {
+        // unwrap because this op will never be called on an empty stack
+        self.tail.as_ref().unwrap().borrow().val
+    }
+
+    pub fn get_min(&self) -> i32 {
+        // unwrap because this op will never be called on an empty stack
+        self.tail.as_ref().unwrap().borrow().min
+    }
 }
 
 #[cfg(test)]
@@ -73,10 +125,9 @@ mod tests {
         min_stack.push(-2);
         min_stack.push(0);
         min_stack.push(-3);
-        println!("{:?}", min_stack);
-        // assert_eq!(min_stack.get_min(), -3);
-        // min_stack.pop();
-        // assert_eq!(min_stack.top(), 0);
-        // assert_eq!(min_stack.get_min(), -2);
+        assert_eq!(min_stack.get_min(), -3);
+        min_stack.pop();
+        assert_eq!(min_stack.top(), 0);
+        assert_eq!(min_stack.get_min(), -2);
     }
 }
