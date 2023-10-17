de533ddf81df092ecbc874f4346222132eace03e
Pavel Pazderník <pazdernik.pavel@email.cz>
2023-10-17 15:24:23 +0200

    Add merge sorted linked lists

--- merge_sorted_linked_lists/src/lib.rs
+++ merge_sorted_linked_lists/src/lib.rs
@@ -1,14 +1,26 @@
 use utils::singly_linked_list::ListNode;
 
-// TODO: https://leetcode.com/problems/merge-two-sorted-lists/
-// Time Complexity: O(?)
-// Space Complexity: O(?)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    None
+    match (list1, list2) {
+        (None, None) => None,
+        (Some(l), None) | (None, Some(l)) => Some(l),
+        (Some(l1), Some(l2)) => match l1.val <= l2.val {
+            true => Some(Box::new(ListNode {
+                val: l1.val,
+                next: merge_two_lists(l1.next, Some(l2)),
+            })),
+            false => Some(Box::new(ListNode {
+                val: l2.val,
+                next: merge_two_lists(Some(l1), l2.next),
+            })),
+        },
+    }
 }
 
 #[cfg(test)]
